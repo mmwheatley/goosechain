9ec4395cb0c42a52fedd7b070e48093c2c60e6b4
mmwheatley <marksaviomw@hotmail.com>
2018-07-18 00:04:58 -0400

    Adds DSAUtils Hash Utils and helper classes

---
+++ src/main/java/goosecoin/utils/SHA3Utils.java
@@ -0,0 +1,70 @@
+package goosecoin.utils;
+
+import goosecoin.crypto.digests.SHA3Digest;
+
+import java.io.UnsupportedEncodingException;
+
+public class SHA3Utils {
+
+    protected enum Size {
+
+        S224(224),
+        S256(256),
+        S384(384),
+        S512(512);
+
+        private int bits;
+
+        Size(int bits) {
+            this.bits = bits;
+        }
+
+        public int getValue() {
+            return this.bits;
+        }
+    }
+
+    private static Size DEFAULT = Size.S224;
+
+    public static byte[] digest(String input) {
+        return digest(input, DEFAULT);
+    }
+
+    public static byte[] digest(byte[] input) {
+        return digest(input, DEFAULT);
+    }
+
+    public static byte[] digest(String string, Size s)  {
+        Size size = s == null ? DEFAULT : s;
+
+        String text = string != null ? string : "null";
+        try {
+            return digest(text.getBytes("UTF-8"), s);
+        } catch (UnsupportedEncodingException ex) {
+            // most unlikely
+            return digest(text.getBytes(), s);
+        }
+    }
+
+    public static byte[] digest(byte[] input, Size s) {
+        SHA3Digest md = new SHA3Digest(s.getValue());
+        byte[] output = new byte[s.getValue()/8];
+        md.update(input, 0, input.length);
+        md.doFinal(output, 0);
+        return output;
+    }
+
+
+/*
+    public static String encode(byte [] bytes, boolean bouncyencoder) {
+        if (bouncyencoder)
+            return Hex.toHexString(bytes);
+        else {
+            BigInteger bigInt = new BigInteger(1, bytes);
+            return bigInt.toString(16);
+        }
+    }
+*/
+
+
+}
