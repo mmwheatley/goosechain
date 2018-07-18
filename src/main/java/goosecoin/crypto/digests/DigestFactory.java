c9288895a54238b7616a837655c9f7a071bc3a54
mmwheatley <marksaviomw@hotmail.com>
2018-07-18 00:05:23 -0400

    Adds SHA3 interface and implementation

---
+++ src/main/java/goosecoin/crypto/digests/DigestFactory.java
@@ -0,0 +1,20 @@
+package goosecoin.crypto.digests;
+
+/*
+public abstract class DigestFactory {
+
+    public enum Algorithm{
+        SHA3 {
+            @Override
+            public Digest create() {
+                return new SHA3Digest();
+            }
+        }
+
+
+    }
+
+    public abstract Digest create();
+}
+
+*/
